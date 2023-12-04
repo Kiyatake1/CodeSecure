a288bf9b36042719948e79087fd8942abc5397bb
Kiyatake1 <otaviokiyatake@gmail.com>
2023-12-04 00:17:35 -0300

    Atualizado pós-sumarização

--- codesecure-connor.py
+++ codesecure-connor.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
-import subprocess, os, threading, json, csv, git, shutil
+import subprocess, os, threading, json, csv, git, shutil, pandas as pd, numpy
 
 def install_security_tools_async():
     install_security_tools()
@@ -11,12 +11,107 @@ def install_security_tools():
     subprocess.run(['sudo', 'apt', 'install', 'npm'])
     subprocess.run(['sudo', 'npm', 'install', '-g', 'snyk'])
 
+#Funções do Sumarizador
+def extract_bandit(n_arquivo:str):
+    arquivo = open(n_arquivo) # opening the JSON file
+    data = json.load(arquivo) # returns JSON object as a dictionary
+
+    lista_description = []
+    lista_severity = []
+    lista_CWE = []
+    lista_code = []
+    lista_file = []
+    lista_line = []
+
+    for analise in data["results"]:
+        lista_description.append(analise["issue_text"])
+        lista_severity.append(analise["issue_severity"])
+        lista_code.append(analise["code"])
+        lista_file.append(analise["filename"])
+        lista_line.append(analise["line_number"])
+        if analise["issue_cwe"]:
+            lista_CWE.append(analise["issue_cwe"]["id"])
+        else:
+            lista_CWE.append("None")
+
+    
+    return lista_description, lista_severity, lista_CWE, lista_code, lista_file, lista_line
+
+def extract_semgrep(n_arquivo:str):
+    arquivo = open(n_arquivo) # opening the JSON file
+    data = json.load(arquivo) # returns JSON object as a dictionary
+
+    lista_description = []
+    lista_severity = []
+    lista_cwe = []
+    lista_code = []
+    lista_file = []
+    lista_line = []
+
+    for analise in data["results"]:
+        lista_description.append(analise["extra"]["metadata"]["cwe"][0])
+        lista_severity.append(analise["extra"]["metadata"]["impact"])
+        lista_cwe.append(analise["extra"]["metadata"]["cwe"][0])
+        lista_code.append(analise["extra"]["lines"])
+        lista_file.append(analise["path"])
+        lista_line.append(analise["start"]["line"])
+    
+    lista_cwe = [i[4:i.index(":")] for i in lista_cwe]
+    lista_description = [i[i.index(":")+2:] for i in lista_description]
+   
+
+    return lista_description, lista_severity, lista_cwe, lista_code, lista_file, lista_line
+
+def sumariza():
+    # Caminho dos Arquivos
+    Semgrep_file = "semgrep_results.json"
+    Bandit_file = "bandit_results.json"
+
+    # Coletando os dados
+    vuls_S, severitys_S, lista_cwe_S, lista_code_S, lista_arquivos_S, lista_linhas_S = [], [], [], [], [], []
+    vuls_B, severitys_B, lista_cwe_B, lista_code_B, lista_arquivos_B, lista_linhas_B = [], [], [], [], [], []
+
+    if os.path.exists(Semgrep_file):
+        vuls_S, severitys_S, lista_cwe_S, lista_code_S, lista_arquivos_S, lista_linhas_S = extract_semgrep(Semgrep_file)
+    if os.path.exists(Bandit_file):
+        vuls_B, severitys_B, lista_cwe_B, lista_code_B, lista_arquivos_B, lista_linhas_B = extract_bandit(Bandit_file)
+
+    # Concatenando os dados
+    vulnerabilidades = vuls_S + vuls_B
+    severidades = severitys_S + severitys_B
+    cwes = lista_cwe_S + lista_cwe_B
+    codigos = lista_code_S + lista_code_B
+    arquivos = lista_arquivos_S + lista_arquivos_B
+    linhas = lista_linhas_S + lista_linhas_B
+
+    # Imprimir informações formatadas
+    print("Análise Completa!")
+    print(f"Vulnerabilidades: {len(vulnerabilidades)}")
+    print(f"Severidades: {len(severidades)}")
+    print(f"CWEs: {len(cwes)}")
+    print(f"Códigos: {len(codigos)}")
+    print(f"Arquivos: {len(arquivos)}")
+    print(f"Linhas: {len(linhas)}")
+    print()
+
+    # Criando o dataframe
+    df = pd.DataFrame()
+    df["Vulnerabilidade"] = vulnerabilidades
+    df["CWE"] = cwes
+    df["Severidade"] = severidades
+    df["Código"] = codigos
+    df["Arquivo"] = arquivos
+    df["Linhas"] = linhas
+
+    df.to_csv("sumarizado.csv")
+
+
 def scan_local(path, selected_sast_tools, selected_sca_tools):
     os.chdir(path)
     results = {}
     if "Bandit" in selected_sast_tools:
-        bandit_result = subprocess.run(["bandit", "-r", "--format", "csv", "."], capture_output=True, text=True)
-        with open('bandit_results.csv', 'w') as csv_file:
+        bandit_result = subprocess.run(["bandit", "-r", "--format", "json", "."], capture_output=True, text=True)
+        with open('bandit_results.json', 'w') as csv_file:
             csv_file.write(bandit_result.stdout)  # Salvando o resultado do Bandit diretamente como CSV
     if "Semgrep" in selected_sast_tools:
         semgrep_result = subprocess.run(["semgrep", "--json", "."], capture_output=True, text=True)
@@ -87,9 +182,13 @@ def create_gui():
     def scan_local_with_tools():
         path = filedialog.askdirectory()
         scan_local(path, selected_sast_tools, selected_sca_tools)
+        sumariza()  # Chama a função para sumarizar após a verificação
+        messagebox.showinfo("Análise Concluída", "A análise foi concluída com sucesso!")
     def scan_github_with_tools():
         repo_url = input("Digite a URL do repositório GitHub: ")
         scan_github(repo_url, selected_sast_tools, selected_sca_tools)
+        sumariza()  # Chama a função para sumarizar após a verificação
+        messagebox.showinfo("Análise Concluída", "A análise foi concluída com sucesso!")
     install_button = ttk.Button(root, text="Instalar Ferramentas de Segurança", command=start_installation)
     install_button.pack(pady=5)
     scan_local_button = ttk.Button(root, text="Escanear Pasta Local", command=scan_local_with_tools)
